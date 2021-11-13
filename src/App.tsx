f717e29e481051287d659ea29a6e5e1e1b53e0ea
Denys <denys.shyshkin@yahoo.com>
2021-11-13 18:05:23 +0200

    Basic navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+import ProfilePage from "./pages/ProfilePage";
+import TrendingPage from "./pages/TrendingPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
 const App = () => {
-  return <div>Project Initialization</div>;
+  return (
+    <div>
+      <header>
+        <nav>
+          <Link to="/">Trending</Link>
+          <Link to="profile">Profile</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<TrendingPage />} />
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
